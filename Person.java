0b91b8a0d01140993666406f251abc523b9f8805
Gokulraj1234 <76981070+Gokulraj1234@users.noreply.github.com>
2021-06-07 23:08:48 +0530

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,175 @@
+import java.util.Scanner;
+class person{
+    Scanner sc=new Scanner(System.in);
+    String gender;
+    String address;
+    String name;
+    int age;
+    person()
+    {
+        System.out.println("Person Details:");
+        System.out.println("Name:");
+        name=sc.next();
+        System.out.println("Address:");
+        address=sc.next();
+        System.out.println("Age:");
+        age=sc.nextInt();
+         System.out.println("Gender:");
+        gender=sc.next();
+    }
+}
+
+class employee extends person
+{
+    int empid;
+    int salary;
+    String company_name;
+    String qualification;
+    Scanner sc=new Scanner(System.in);
+    employee()
+    {
+        System.out.println("Empolyee Details");
+        System.out.println("Company Name:");
+        company_name=sc.next();
+        System.out.println("Employee ID:");
+        empid=sc.nextInt();
+        System.out.println("Qualification:");
+        qualification=sc.next();
+        System.out.println("Salary:");
+        salary=sc.nextInt();
+    }
+}
+class teacher extends employee
+{
+    Scanner sc=new Scanner(System.in);
+    String dept;
+    String sub;
+    int techrID;
+    teacher(){
+        System.out.println("Teacher Details:");
+        System.out.println("Department name:");
+        dept=sc.next();
+        System.out.println("Subject:");
+        sub=sc.next();
+        System.out.println("Teahcer ID:");
+        techrID= sc.nextInt();
+    }
+    void display()
+    {
+    	System.out.println(" ");
+        System.out.println("Details are:");
+        System.out.println(" ");
+        System.out.println("Name:"+name);
+        System.out.println("Age:"+age);
+        System.out.println("Gender:"+gender);
+        System.out.println("Company Name:"+company_name);
+        System.out.println("EmpId:"+empid);
+        System.out.println("Qualification:"+qualification);
+        System.out.println("TeacherId:"+techrID);
+        System.out.println("Address:"+address);
+        System.out.println("Subject:"+sub);
+        System.out.println("Department:"+dept);
+        System.out.println("Salary:"+salary);
+    }
+}
+class persn 
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the limit:");
+        int n=sc.nextInt();
+        teacher tr[]=new teacher[n];
+        for (int i=0;i<n;i++)
+        {
+            tr[i]=new teacher();
+            tr[i].display();
+        }
+    }
+}
+
+===========================================================
+Output:
+
+C:\Users\Dell\Desktop>java persn
+Enter the limit:
+2
+Person Details:
+Name:
+Gokul
+Address:
+ABHouse
+Age:
+20
+Gender:
+Male
+Empolyee Details
+Company Name:
+CHash
+Employee ID:
+11211
+Qualification:
+MCA
+Salary:
+25000
+Teacher Details:
+Department name:
+MCA
+Subject:
+Python
+Teahcer ID:
+100121
+
+Details are:
+
+Name:Gokul
+Age:20
+Gender:Male
+Company Name:CHash
+EmpId:11211
+Qualification:MCA
+TeacherId:100121
+Address:ABHouse
+Subject:Python
+Department:MCA
+Salary:25000
+
+Person Details:
+Name:
+Akhil
+Address:
+CHHouse
+Age:
+20
+Gender:
+Male
+Empolyee Details
+Company Name:
+Dotnet
+Employee ID:
+1001121
+Qualification:
+BCA
+Salary:
+20000
+Teacher Details:
+Department name:
+BCA
+Subject:
+C
+Teahcer ID:
+121121
+
+Details are:
+
+Name:Akhil
+Age:20
+Gender:Male
+Company Name:Dotnet
+EmpId:1001121
+Qualification:BCA
+TeacherId:121121
+Address:CHHouse
+Subject:C
+Department:BCA
+Salary:20000
