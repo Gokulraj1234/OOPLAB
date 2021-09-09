f3695067d1f114d5633ed245cafa02aa25694d4e
Gokulraj1234 <76981070+Gokulraj1234@users.noreply.github.com>
2021-09-09 09:30:25 +0530

    Update Sorted.java

--- cy3q7/Sorted.java
+++ cy3q7/Sorted.java
@@ -1,58 +1,77 @@
-import java.util.*;
+import java.util.Scanner;
 
-class  stringSort
-{
-    String[] s= new String[100];
-    int len;
-    void getStr()
+class SortString
+ {
+  String[] str = new String[100];
+  int size;
+
+ void get() 
+  {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter limit:");
+    size = sc.nextInt();
+    System.out.println("Enter the String:");
+     for (int i=0;i< size;i++)
+     {
+    
+      str[i] = sc.next();       
+    } 
+  }
+
+ void sortstring()
+  {
+   for (int i = 0; i < size; i++)
+   {
+    for (int j = i+1; j < size; j++)
     {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter limit:");
-        len = sc.nextInt();
-        System.out.println("Enter elements:");
-        for (int i=0;i< len;i++)
-        {
-            s[i] = sc.next();
-        }
-    }
-    void strSort()
+     if (str[i].length() < str[j].length())
+     {
+       String temp = str[i];
+       str[i] = str[j];
+       str[j] = temp;  
+      } 
+    } 
+  }
+ }
+
+ void sorted()
+  {  
+     System.out.print("-----------------------------\n");
+    System.out.println("Descending Order:");
+    for (int i=0;i< size;i++)
     {
-        for (int i=0;i<len-1;i++)
-        {
-            for (int j=i+1;j<len;j++)
-            {
-                if(s[i].compareTo(s[j])>0)
-                {
-                    String temp = s[i];
-                    s[i] = s[j];
-                    s[j] = temp;
-                }
-            }
-        }
-    }
-    void dispStr()
+     System.out.println(str[i]);  
+   }
+        
+  for (int i = 0; i <size; i++)
+  {
+         for (int j = i+1; j <size; j++)
+         {
+          if (str[i].length() > str[j].length())
+          {
+           String temp = str[i];
+           str[i] = str[j];
+           str[j] = temp; 
+         } 
+       }
+      } 
+
+   System.out.println("\nAscending Order:");
+    for (int i=0;i< size;i++)
     {
-        System.out.println("\n---------------------");
-        System.out.println("Ascending order:");
-        for (int i=0;i< len;i++)
-        {
-            System.out.println(s[i]);
-        }
-        System.out.println("\nDescending order:");
-        for (int i=len-1;i>=0;i--)
-        {
-            System.out.println(s[i]);
-        }
-    }
+     System.out.println(str[i]); 
+   }    
+   
+   }
 }
 
-public class Sorted
+class Main
 {
-    public static void main(String args[])
-    {
-        stringSort s = new stringSort();
-        s.getStr();
-        s.strSort();
-        s.dispStr();
-    }
+  public static void main(String[] args) 
+  {
+   SortString s=new SortString();
+    s.get();
+    s.sortstring();
+    s.sorted();
+  }
 }
