80ba6733324be492f7ae55dcb77b755faa4afbc4
Gokulraj1234 <76981070+Gokulraj1234@users.noreply.github.com>
2021-09-20 17:50:59 +0530

    Add files via upload

---
+++ cy4 q2/filewriter.java
@@ -0,0 +1,39 @@
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.IOException;
+
+class ReadWrite 
+{
+
+	public static void main(String[] args)
+	{
+
+		try {
+
+			FileWriter fwrite = new FileWriter("writingfile.txt");
+			fwrite.write("Kerala in the South-Western region of India is often referred to as God own country.\n");
+			fwrite.write("This beautiful land is rightly called the paradise.\n");
+			fwrite.write("Blessed with natural beauty and ecological diversity.\n");
+			fwrite.write("Kerala has a lot to offer to tourists and travelers.");
+			//System.out.println("File write done\n");
+		    fwrite.close();
+			FileReader fread = new FileReader("writingfile.txt");
+			String str="";
+            int i;
+			while ((i=fread.read())!=-1)
+	        {
+              str+=(char)i;
+            }
+            System.out.println(str);
+			//System.out.println("\nFile read done");
+            fread.close();
+
+		}
+
+		catch (IOException e) 
+		{
+
+			System.out.println("There are some IOException");
+		}
+	}
+}
